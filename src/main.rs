c3745b881f577dc250818b903bb3ba034b87de0a
Abcight <abcightdev@gmail.com>
2022-03-25 20:16:29 +0100

    made it more idiomatic

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 pub mod api;
 
 fn solve(words: &mut std::vec::Vec<&str>) {
-	if let Some(word) = words.clone().get(0) {
+	if let Some(word) = words.get(0) {
+		let word = word.to_owned();
 		let res = api::try_guess(word);
-
+		
 		println!("{} {:?}", word, res);
 		
 		if res.iter().sum::<u8>() == 10 {
 			return;
 		}
-
+		
 		for (i, score) in api::try_guess(word).iter().enumerate() {
 			let ch = word.chars().nth(i).unwrap();
 			match score {
@@ -18,7 +19,6 @@ fn solve(words: &mut std::vec::Vec<&str>) {
 				_ => words.retain(|word| word.chars().nth(i).unwrap() == ch)
 			}
 		}
-
 		solve(words);
 	}
 }
